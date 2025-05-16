c664187d5b528604a6f3aa5ac5cdbf466799bc6b
Nurkhalisa Rahmadani <rahmadaninurkhalisa@gmail.com>
2025-05-16 11:00:14 +0700

    Add files via upload

---
+++ pertemuan 7 nurkholisa/soal1.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Nurkhalisa';
+let lastName = 'Rahmadani';
+let bornYear = 2005;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
